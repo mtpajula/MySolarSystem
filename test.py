b3443769abbe5afe169f69528e8e66cd838e4903
leevi <leevi@HP-dc7700.(none)>
2012-04-25 14:36:13 +0300

    tests follow document

--- test.py
+++ test.py
@@ -7,11 +7,12 @@ from universe.uni_object import Uni_Object
 from universe.uni_object import Uni_Object
 from gui.paint import Helper
 import math
+import os
 
 class Test(unittest.TestCase):
     '''
     
-    @author: Mikko Pajula 64570K
+    @author: Mikko Pajula
     
     '''
 
@@ -93,17 +94,94 @@ class Test(unittest.TestCase):
         
         self.assertEqual(500 ,round(obj.x),  "Wrong x location")
         
+    def test_move_2(self):
+        '''
+        Test: Moving with gravity
+        '''
+        self.controller.universe.maths.time = 100000
+        
+        obj1 = self.controller.create_object("test1",5000000,1)
+        obj2 = self.controller.create_object("test2",5000000,1)
+        obj1.set_location(-100,0,0)
+        obj2.set_location(100,0,0)
+        
+        self.controller.animate_step()
+        
+        r = math.sqrt(200**2 + 0**2)
+        f = 6.67 * 10**-11 # Nm^2/kg^2
+        speed1 = f * ( ( 100000 * obj2.mass ) / r**2 )
+        lenght = speed1 * 100000
+        
+        obj1_travel = 100 + obj1.x
+        
+        self.assertEqual(round(lenght,1) ,round(obj1_travel,1),  "wrong distance moved")
+        self.assertEqual(round(obj1.x,1) ,(-1*round(obj2.x,1)), "Both should move equally long distance")
+        
+    def test_move_3(self):
+        '''
+        Test: Moving with custom force vector
+        '''
+        self.controller.universe.maths.time = 50
+        obj = self.controller.create_object("test",10,1)
+        
+        self.controller.create_angle_force(obj,1,0,0)
+        
+        f = 1.0
+        m = 10.0
+        t = 50.0
+        
+        a = f / m
+        v = a * t
+        s = v * t
+        
+        self.controller.animate_step()
+        self.assertEqual(s ,round(obj.x),  "Wrong x location")
+        
     def test_line_direction(self):
         '''
         Test: Draw line (lenght x) to same direction as given line
         '''
-        background_color = QtGui.QColor(64, 32, 64)
-        helper = Helper(self.controller, background_color)
+        color = QtGui.QColor(64, 32, 64)
+        helper = Helper(self.controller, color, color)
         
-        (x,y) = helper.line_direction(20,5)
+        (x,y) = helper.line_direction(500,-500,0)
         
         lenght = math.sqrt(x**2 + y**2)
         self.assertEqual(10 ,round(lenght),  "Wrong lenght")
+        
+    def test_save_load(self):
+        '''
+        test file save and load
+        '''
+        
+        obj = self.controller.create_object("test",10,2)
+        self.controller.create_angle_force(obj,1,0,0)
+        
+        self.controller.file_name = 'test_py.xml'
+        
+        self.controller.save()
+        
+        self.controller.new_controller()
+        
+        self.assertEqual(False ,self.controller.not_empty(),  "Should be empty universe")
+        
+        self.controller.file_name = 'test_py.xml'
+        self.controller.load()
+        filePath = self.controller.folder + self.controller.file_name
+        os.remove(filePath)
+        
+        
+        self.assertEqual(True ,self.controller.not_empty(),  "Should not be empty universe")
+        
+        loadObj = self.controller.universe.object_list[0]
+        
+        self.assertEqual('test' ,loadObj.name,  "wrong object paramenters")
+        self.assertEqual(10 ,loadObj.mass,  "wrong object paramenters")
+        self.assertEqual(2 ,loadObj.radius,  "wrong object paramenters")
+        
+        loadForce = loadObj.force_vector_list[0]
+        
+        self.assertEqual(1 ,loadForce.x,  "wrong force object paramenters")
 
 if __name__ == "__main__":
     unittest.main()
